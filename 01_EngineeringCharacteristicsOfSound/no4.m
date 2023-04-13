dc2599bc3e4be0fb73970fcd444b8b516eca3f28
MIZOGUCHI Koki <94270819+MIZOGUCHIKoki@users.noreply.github.com>
2023-04-13 14:26:44 +0900

    change range of for variable

--- 01_EngineeringCharacteristicsOfSound/no4.m
+++ 01_EngineeringCharacteristicsOfSound/no4.m
@@ -17,19 +17,19 @@ axis([0 2/f -3 3]);
 title('N=1');
 
 y = 0;
-for k=1:25
+for k=1:5
     y = y + (1/(2*k-1)) * sin(2*pi*f*(2*k-1)*t);
 end
 subplot(3,1,2);
 plot(t,y);
 axis([0 2/f -3 3]);
-title('N=25');
+title('N=5');
 
 y = 0;
-for k=1:50
+for k=1:25
     y = y + (1/(2*k-1)) * sin(2*pi*f*(2*k-1)*t);
 end
 subplot(3,1,3);
 plot(t,y);
 axis([0 2/f -3 3]);
-title('N=50');
+title('N=25');
