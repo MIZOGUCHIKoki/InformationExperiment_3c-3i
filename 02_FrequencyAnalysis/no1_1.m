90247b03bf635a661df8a90e032926083b966e67
MIZOGUCHI Koki <94270819+MIZOGUCHIKoki@users.noreply.github.com>
2023-04-14 15:01:43 +0900

    Update

--- 02_FrequencyAnalysis/no1_1.m
+++ 02_FrequencyAnalysis/no1_1.m
@@ -4,15 +4,28 @@ clc;
 Fs = 8192;
 t = [0:(Fs-1)] / Fs;
 f = 440;
-
+fs=[-Fs/2:Fs/1024:(Fs/2)-1];
 y = sin(2*pi*f*t);
 
 
 y = y(1,1:1024);
 fft_y = fft(y);
-fft_ys = fftshift(fft_y);
-fft_ys = abs(fft_ys);
-fs=[-Fs/2:Fs/1024:(Fs/2)-1];
-plot(fs,fft_ys);
+
+figure;
+plot(fs,fft_y);
+title('ftt');
 xlabel('周波数');
-axis([-1000 1000 0 600]);
+
+fft_ys = fftshift(fft_y);
+figure;
+plot(fs,fft_ys);
+title('fttshift');
+xlabel('周波数');
+
+
+fft_ys = abs(fft_ys);
+figure;
+plot(fs,fft_ys);
+title('abs');
+xlabel('周波数');
+% axis([-1000 1000 0 600]);
