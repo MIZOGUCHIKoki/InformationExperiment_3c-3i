b0b9134387fe0fd7ec2f744d56c5a033c080b2bd
MIZOGUCHI Koki <94270819+MIZOGUCHIKoki@users.noreply.github.com>
2023-04-26 18:24:49 +0900

    Update

--- 02_FrequencyAnalysis/no2.m
+++ 02_FrequencyAnalysis/no2.m
@@ -23,43 +23,45 @@ y_6 = sin(2 * pi * f6 * t);
 y_7 = sin(2 * pi * f7 * t);
 y = [y_0 y_1 y_2 y_3 y_4 y_5 y_6 y_7];
 
-figure;
+fig0 = figure;
 plot(t8,y);
 xticks(0:0.25:2);
 xlabel('time[s]');
-ylabel('振幅');
+ylabel('amplitude');
+axis([0 2 -1.5 1.5]);
 
 fft_y = fft(y);
 fft_y = fftshift(fft_y);
 f = (-Fs/2 : Fs/length(fft_y)  : Fs/2 - Fs/length(fft_y));
 
-figure;
+fig1 = figure;
 subplot(2,1,1);
 plot(f,fft_y);
-xlabel('周波数[Hz]');
-ylabel('振幅');
+xlabel('frequency[Hz]');
+ylabel('amplitude');
 axis([-1000 1000 -1500 1500]);
-
 
 for k=1:length(fft_y)
     if f(k) >= 375.00 || f(k) <= -375.00
         fft_y(k) = 0;
     end
 end
-
-
 subplot(2,1,2);
 plot(f,fft_y);
 axis([-1000 1000 -1500 1500]);
-xlabel('周波数[Hz]');
-ylabel('振幅');
+xlabel('frequency[Hz]');
+ylabel('amplitude');
 ifft_y = ifftshift(fft_y);
 ifft_y = ifft(ifft_y);
 ifft_real = real(ifft_y);
 
-figure;
+fig2 = figure;
 plot(t8,ifft_y);
 xlabel('time[s]');
-ylabel('振幅');
+ylabel('amplitude');
 xticks(0:0.25:2);
-sound(ifft_real,Fs)
+axis([0 2 -1.5 1.5]);
+% sound(ifft_real,Fs)
+exportgraphics(fig0,'../Figures/02_20.pdf','ContentType','vector');
+exportgraphics(fig1,'../Figures/02_21.pdf','ContentType','vector');
+exportgraphics(fig2,'../Figures/02_22.pdf','ContentType','vector');
