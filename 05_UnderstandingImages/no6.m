44d2fee63317670b6b665e4f55df542e2686e918
MIZOGUCHI Koki <94270819+MIZOGUCHIKoki@users.noreply.github.com>
2023-04-27 15:56:19 +0900

    Update

--- 05_UnderstandingImages/no6.m
+++ 05_UnderstandingImages/no6.m
@@ -1,8 +1,13 @@
 clear;
+
 img1 = imread("fig1.jpg"); % 人あり
 img2 = imread("fig2.jpg"); % 人なし
+img1 = 0.3*img1(:,:,1) + 0.59*img1(:,:,2) + 0.11*img1(:,:,3);
+img2 = 0.3*img2(:,:,1) + 0.59*img2(:,:,2) + 0.11*img2(:,:,3);
 
-result = img2 - img1;
+result = int16(img2) - int16(img1);
+result = abs(result);
+result = uint8(result);
 figure;
 imshow(result);
 imwrite(result,"../Figures/05_6.png");
