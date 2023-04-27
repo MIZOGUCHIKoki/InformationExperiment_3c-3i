f8c350f95443514bbbaf578bb0628eaf94c033d1
MIZOGUCHI Koki <94270819+MIZOGUCHIKoki@users.noreply.github.com>
2023-04-27 16:14:43 +0900

    Update

--- 05_UnderstandingImages/no2.m
+++ 05_UnderstandingImages/no2.m
@@ -4,9 +4,10 @@ gimg = 0.3*img(:,:,1) + 0.59*img(:,:,2) + 0.11*img(:,:,3);
 figure;
 imshow(gimg);
 imwrite(gimg,"../Figures/05_21gimg.png");
-gray_4bit = bitshift(gimg,-4) * 16;  % XXXX XXXX -> XXXX
-gray_2bit = bitshift(gimg,-6) * 128; % XXXX XXXX -> XX
-gray_1bit = bitshift(gimg,-7) * 255; % XXXX XXXX -> X
+gray_4bit = bitshift(gimg,-4) * (255/15);  % XXXX XXXX -> XXXX
+gray_2bit = bitshift(gimg,-6) * (255/3);   % XXXX XXXX -> XX
+gray_1bit = bitshift(gimg,-7) * (255/1);   % XXXX XXXX -> X
+
 figure;
 imshow(gray_4bit);
 imwrite(gray_4bit,"../Figures/05_224bit.png");
