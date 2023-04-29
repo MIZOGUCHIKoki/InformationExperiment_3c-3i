6b9dd389378a57dc0fb7cd446d4afb9813a2b422
MIZOGUCHI Koki <94270819+MIZOGUCHIKoki@users.noreply.github.com>
2023-04-30 06:33:28 +0900

    Update

--- 03_SpeechSynthesis/no2_i.m
+++ 03_SpeechSynthesis/no2_i.m
@@ -6,30 +6,17 @@ t = (1:N) /Fs; % 時間
 z = y.*(-1);
 
 soundsc(y,Fs);
-pause(3);
+pause(5);
 soundsc(z,Fs);
-%プロット
-figure;
-subplot(2,1,1);
-plot(t,y);
-xlabel('time[s]');
-ylabel('waveform');
-title('1) 「あ」の波形（一部抜粋）');
-axis([0 0.01 -0.04 0.04]);
 
-subplot(2,1,2);
-plot(t,z);
-xlabel('time[s]');
-ylabel('waveform');
-title('2) 1)の正負反転波形');
-axis([0 0.01 -0.04 0.04]);
-
-figure;
+fig = figure;
 hold on;
 plot(t,y);
 plot(t,z);
-legend({'1) 「あ」の波形（一部抜粋）','2) 1)の正負反転波形'},'Location','southwest');
+plot(t,y+z);
+hold off;
+legend({'1) original wave','2) sign inversion wave','3) synthetic wave'},'Location','southwest');
 xlabel('time[s]');
-ylabel('waveform');
-title('表示を合成');
+ylabel('amplitude');
 axis([0 0.01 -0.04 0.04]);
+exportgraphics(fig,'../Figures/03_2_i.pdf','ContentType','vector');
