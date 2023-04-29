44bf88422060d80fba9e07ed754ec9b1222a5ce1
MIZOGUCHI Koki <94270819+MIZOGUCHIKoki@users.noreply.github.com>
2023-04-30 02:47:37 +0900

    Update

--- 03_SpeechSynthesis/no1_1.m
+++ 03_SpeechSynthesis/no1_1.m
@@ -1,21 +1,20 @@
-clear all;
-% clc;
-Fs = 16000; % サンプリング周波数
-f = 440; % 基本周波数
-t = (0 : 2*(Fs-1)) /Fs; % 時刻
-phi1 = pi / 4;
-phi2 = pi / 2;
+clear;
+Fs = 16000;
+f = 440;
+t = (0:2*Fs-1) /Fs;
+phi1 = pi/4;
+phi2 = pi/2;
 
-y1 = 0;
-y2 = 0;
-y3 = 0;
-y4 = 0;
-
-for k = 1 : 500
-    y1 = y1 + (-1)^(k-1) * 1/3 * 2/k * sin(2*pi*f*k*t);       % 級数を表現
-    y2 = y2 + (-1)^(k-1) * 1/3 * 2/k * sin(2*pi*f*k*t+phi1);
-    y3 = y3 + (-1)^(k-1) * 1/3 * 2/k * sin(2*pi*f*k*t+phi2);
-    y4 = y4 + (-1)^(k-1) * 1/3 * 2/k * sin(2*pi*f*k*t+rand);
+y1=0;
+y2=0;
+y3=0;
+y4=0;
+% 矩形波
+for k = 1:50
+    y1 = y1 + 1/(2*k-1) * sin(2*pi*f*(2*k-1)*t);
+    y2 = y2 + 1/(2*k-1) * sin(2*pi*f*(2*k-1)*t+phi1);
+    y3 = y3 + 1/(2*k-1) * sin(2*pi*f*(2*k-1)*t+phi2);
+    y4 = y4 + 1/(2*k-1) * sin(2*pi*f*(2*k-1)*t+rand);
 end
 
 sound(y1,Fs);
@@ -27,38 +26,31 @@ pause(3);
 sound(y4,Fs);
 pause(3);
 
-figure;
+fig0 = figure;
 plot(t,y1);
 xlabel('time[s]');
-ylabel('waveform');
-title('通常ノコギリ波');
+ylabel('amplitude');
 axis([0 0.01 -3 3]);
-%プロット
-figure;
-subplot(4,1,1);
-plot(t,y1);
-xlabel('time[s]');
-ylabel('waveform');
-title('通常ノコギリ波');
-axis([0 0.01 -3 3]);
+exportgraphics(fig0,'../Figures/03_11_kukei.pdf','ContentType','vector');
 
-subplot(4,1,2);
+fig1 = figure;
 plot(t,y2);
 xlabel('time[s]');
-ylabel('waveform');
-title('π/4位相ずれ');
+ylabel('amplitude');
 axis([0 0.01 -3 3]);
-subplot(4,1,3);
+exportgraphics(fig1,'../Figures/03_12.pdf','ContentType','vector');
 
+
+fig2 = figure;
 plot(t,y3);
 xlabel('time[s]');
-ylabel('waveform');
-title('π/2位相ずれ');
+ylabel('amplitude');
 axis([0 0.01 -3 3]);
+exportgraphics(fig2,'../Figures/03_13.pdf','ContentType','vector');
 
-subplot(4,1,4);
+fig3 = figure;
 plot(t,y4);
 xlabel('time[s]');
-ylabel('waveform');
-title('ランダム位相ずれ');
+ylabel('amplitude');
 axis([0 0.01 -3 3]);
+exportgraphics(fig3,'../Figures/03_14.pdf','ContentType','vector');
--- 03_SpeechSynthesis/no1_2.m
+++ 03_SpeechSynthesis/no1_2.m
@@ -1,55 +1,55 @@
-%やっていることはkadai1_1とほぼ同じなのでそちらを参照
+clear;
+Fs = 16000; % サンプリング周波数
+f = 440; % 基本周波数
+t = (0 : 2*Fs-1) /Fs; % 時刻
+phi1 = pi / 4;
+phi2 = pi / 2;
 
-clear all;
-
-Fs=16000;
-f=440;
-t=[0:2*(Fs-1)]/Fs;
-phi1=pi/4;
-phi2=pi/2;
-phi3=rand;
-
-y1=0;
-y2=0;
-y3=0;
-y4=0;
-for k=1:2:50;
-    y1=y1+1/k*sin(2*pi*f*k*t);
-    y2=y2+1/k*sin(2*pi*f*k*t+phi1);
-    y3=y3+1/k*sin(2*pi*f*k*t+phi2);
-    y4=y4+1/k*sin(2*pi*f*k*t+phi3);
+y1 = 0;
+y2 = 0;
+y3 = 0;
+y4 = 0;
+% ノコギリ波
+for k = 1 : 50
+    y1 = y1 + (-1)^(k-1) * 1/3 * 2/k * sin(2*pi*f*k*t);       % 級数を表現
+    y2 = y2 + (-1)^(k-1) * 1/3 * 2/k * sin(2*pi*f*k*t+phi1);
+    y3 = y3 + (-1)^(k-1) * 1/3 * 2/k * sin(2*pi*f*k*t+phi2);
+    y4 = y4 + (-1)^(k-1) * 1/3 * 2/k * sin(2*pi*f*k*t+rand);
 end
 
-soundsc(y1,Fs);
+sound(y1,Fs);
 pause(3);
-soundsc(y2,Fs);
+sound(y2,Fs);
 pause(3);
-soundsc(y3,Fs);
+sound(y3,Fs);
 pause(3);
-soundsc(y4,Fs);
+sound(y4,Fs);
 pause(3);
 
-subplot(4,1,1);
+fig1 = figure;
 plot(t,y1);
 xlabel('time[s]');
-ylabel('waveform');
-title('通常短形波');
+ylabel('amplitude');
 axis([0 0.01 -3 3]);
-subplot(4,1,2);
+exportgraphics(fig1,'../Figures/03_21_nokogiri.pdf','ContentType','vector');
+
+fig2 = figure;
 plot(t,y2);
 xlabel('time[s]');
-ylabel('waveform');
-title('π/4位相ずれ');
+ylabel('amplitude');
 axis([0 0.01 -3 3]);
-subplot(4,1,3);
+exportgraphics(fig2,'../Figures/03_22.pdf','ContentType','vector');
+
+fig3 = figure;
 plot(t,y3);
 xlabel('time[s]');
-ylabel('waveform');
-title('π/2位相ずれ');
+ylabel('amplitude');
 axis([0 0.01 -3 3]);
-subplot(4,1,4);
+exportgraphics(fig3,'../Figures/03_23.pdf','ContentType','vector');
+
+fig4 = figure;
 plot(t,y4);
 xlabel('time[s]');
-ylabel('waveform');
-title('ランダム位相ずれ');
+ylabel('amplitude');
 axis([0 0.01 -3 3]);
+exportgraphics(fig4,'../Figures/03_24.pdf','ContentType','vector');
