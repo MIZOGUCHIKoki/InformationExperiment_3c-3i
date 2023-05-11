cf5b98bbb39792164096a2ed8e9b91e03965c6b1
MIZOGUCHI Koki <94270819+MIZOGUCHIKoki@users.noreply.github.com>
2023-05-11 15:08:01 +0900

    Update

---
+++ 07_DirectionAftereffect/no1.m
@@ -0,0 +1,30 @@
+clear; close all;
+C = 0.5; % コントラスト（今回は0.5）
+f = 0.05; % 空間周波数（今回は0.05）
+d1 = (-1/6)*pi;
+d2 = (1/3)*pi;
+L_0 = 128;
+height = 300;
+width = 300;
+L_l30 = zeros(height,width);
+L_r60 = zeros(height,width);
+
+
+for x = 1:width
+    for y = 1:height
+        L_l30(y,x) = L_0 * (1 + C * sin(2 * pi * f*(y*sin(d1)+x*cos(d1))));
+        L_r60(y,x) = L_0 * (1 + C * sin(2 * pi * f*(y*sin(d2)+x*cos(d2))));
+    end
+end
+
+figure;
+colormap(gray(256));
+image(L_l30);
+axis off;
+axis image;
+
+figure;
+colormap(gray(256));
+image(L_r60);
+axis off;
+axis image;
