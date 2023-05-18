959f7582a2a5ae470c57c9cd067a67c339f80260
MIZOGUCHI Koki <94270819+MIZOGUCHIKoki@users.noreply.github.com>
2023-05-19 02:31:02 +0900

    Partilly complete Result

--- 08_FourierAnalysisOfImages/no3.m
+++ 08_FourierAnalysisOfImages/no3.m
@@ -3,26 +3,30 @@ H = 256;
 W = 256;
 img1 = zeros(H,W);
 img1(H/2-30:H/2+30,W/2-15:W/2+15) = 255;
-figure;
+fig0 = figure;
 imshow(img1);
+axis off;
+exportgraphics(fig0,'../Figures/08_31_rec1.pdf','ContentType','vector');
 img2 = zeros(H,W);
 img2(H/2+60-30:H/2+60+30,W/2+60-15:W/2+60+15) = 255;
-figure;
+fig0 = figure;
 imshow(img2);
+axis off;
+exportgraphics(fig0,'../Figures/08_32_rec2.pdf','ContentType','vector');
 
 img1_fft = log(1 + abs(fftshift(fft2(img1))).^2);
-img2_fft = abs(fftshift(fft2(img2))).^2;
+img2_fft = log(1 + abs(fftshift(fft2(img2))).^2);
 
 fig0 = figure;
 colormap('gray');
 imagesc(img1_fft);
-xlabel('x: frequency');
-ylabel('y: frequency');
-fig1 = figure;
+axis off;
+exportgraphics(fig0,'../Figures/08_33_rec1-fft.pdf','ContentType','vector');
+fig0 = figure;
 colormap('gray');
 imagesc(img2_fft);
-xlabel('x: frequency');
-ylabel('y: frequency');
+axis off;
+exportgraphics(fig0,'../Figures/08_34_rec2-fft.pdf','ContentType','vector');
 
 diff = img2_fft - img1_fft;
 diff_max = max(max(diff));
--- 08_FourierAnalysisOfImages/no4.m
+++ 08_FourierAnalysisOfImages/no4.m
@@ -17,28 +17,33 @@ for y = 1:H
         end
     end
 end
-figure;
+fig0 = figure;
 imshow(filter_50);
+axis off;
+exportgraphics(fig0,'../Figures/08_41_filter.pdf','ContentType','vector');
 img_fft = fftshift(fft2(img));
 fig0 = figure;
 colormap('gray');
 imagesc(log(1 + abs(img_fft).^2));
-xlabel('x: frequency');
-ylabel('y: frequency');
+axis off;
+exportgraphics(fig0,'../Figures/08_42_fft.pdf','ContentType','vector');
 
 img_filter50 = img_fft.*filter_50;
 img_filter100 = img_fft.*filter_100;
 img_ifilter50 = real(ifft2(ifftshift(img_filter50)));
 img_ifilter100 = real(ifft2(ifftshift(img_filter100)));
-fig1 = figure;
+fig0 = figure;
 colormap('gray');
 imagesc(log(1 + abs(img_filter50).^2));
-xlabel('x: frequency');
-ylabel('y: frequency');
-
-fig2 = figure;
+axis off;
+exportgraphics(fig0,'../Figures/08_43_fft-filter.pdf','ContentType','vector');
+fig0 = figure;
 colormap('gray');
 imagesc(img_ifilter50);
-fig3 = figure;
+axis off;
+exportgraphics(fig0,'../Figures/08_44_fft-filter-50.pdf','ContentType','vector');
+fig0 = figure;
 colormap('gray');
 imagesc(img_ifilter100);
+axis off;
+exportgraphics(fig0,'../Figures/08_45_fft-filter-100.pdf','ContentType','vector');
