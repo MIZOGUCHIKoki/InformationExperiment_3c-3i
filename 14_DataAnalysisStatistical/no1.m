d63f54c366ecd59fa910bed28e6cce90723a90f3
MIZOGUCHI Koki <94270819+MIZOGUCHIKoki@users.noreply.github.com>
2023-06-08 15:49:26 +0900

    Updat

---
+++ 14_DataAnalysisStatistical/no1.m
@@ -0,0 +1,25 @@
+clear; close all;
+d = importdata('data.csv',',',3);
+data = d.data;
+data(:,1) = [];
+
+[people, setnum] = size(data);
+ave_data = mean(data); % 各列の期待値
+var_data = var(data); % 各列の不偏分散
+std_data = std(data); % 各列の標準偏差
+err_data = std_data/sqrt(people); % 標準誤差
+setsize = [4 8 16];
+fig1 = figure;
+hold on;
+errorbar(setsize, ave_data(1:3), err_data(1:3),'-',LineWidth=1); % YES / C
+errorbar(setsize, ave_data(7:9), err_data(7:9),'-',LineWidth=1); % YES / CL
+errorbar(setsize, ave_data(4:6), err_data(4:6),'-',LineWidth=1); % NO / C
+errorbar(setsize, ave_data(10:12), err_data(10:12),'-',LineWidth=1); % NO / CL
+hold off;
+axis([3 17 600 2000]);
+xticks([4 8 16])
+lgd = legend({'YES / C','YES / CL','NO / C', 'NO / CL'},'Location','northwest');
+title(lgd,'Target / Target type');
+xlabel('Set size');
+ylabel('Response time average [ms]');
+exportgraphics(fig1,'../Figures/14_01_graph.pdf','ContentType','vector');
---
+++ 14_DataAnalysisStatistical/no2.m
@@ -0,0 +1,39 @@
+clear; close all;
+d = importdata('data.csv',',',3);
+data = d.data;
+data(:,1) = [];
+[people, setnum] = size(data);
+setsize = [4 8 16];
+x = setsize;
+slope = zeros(people, 3);
+for k=1:people
+    y = data(k,1:3);
+    p_circle = polyfit(x,y,1);
+    slope(k,1) = p_circle(1);
+    y = data(k,7:9);
+    p_circle = polyfit(x,y,1);
+    slope(k,2) = p_circle(1);
+    slope(k,3) = p_circle(1) - p_circle(2);
+end
+mynum = 2;
+fig1 = figure;
+hold on;
+y = data(mynum,1:3);
+plot(x,y,'o',LineWidth=2);
+p_circle = polyfit(x,y,1);
+a = p_circle(:,1);
+b = p_circle(:,2);
+y1 = a * x + b;
+y = data(mynum, 7:9);
+plot(x,y,'x',LineWidth=2);
+p_circle_bar = polyfit(x,y,1);
+a = p_circle_bar(:,1);
+b = p_circle_bar(:,2);
+y2 = a * x + b;
+plot(x, y1, x, y2, LineWidth=2);
+leg = legend('Data point / C','Data point / CL', 'Regression line / C', 'Regression line / CL','Location','northwest');
+title(leg, 'Genre / Target type')
+axis([3 17 900 1600]);
+xticks([4 8 16])
+hold off;
+exportgraphics(fig1,'../Figures/14_02_graph.pdf','ContentType','vector');
